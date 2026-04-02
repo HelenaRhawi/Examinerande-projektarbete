4d13cdcd54d0105d228963acbb77c29215b8b192
tacokrydda <jbtand19@gmail.com>
2026-04-02 13:44:10 +0200

    wip

--- middleware/validateOptionalUser.js
+++ middleware/validateOptionalUser.js
@@ -3,18 +3,19 @@ import db from "../data/db.js";
 import { validate as validateUUID } from "uuid";
 
 export default function validateOptionalUser(req, res, next) {
-  const { id } = req.body;
+  const { userId } = req.body;
 
-  if (!id) {
+
+  if (!userId) {
     req.user = null;
     return next();
   }
 
-  if (!validateUUID(id)) {
+  if (!validateUUID(userId)) {
     return res.status(400).json({ Error: "Invalid ID-format" });
   }
 
-  const user = db.prepare("SELECT * FROM users WHERE id = ?").get(id);
+  const user = db.prepare("SELECT * FROM users WHERE id = ?").get(userId);
 
   if (!user) {
     return res.status(404).json({ Error: "User not found" });
