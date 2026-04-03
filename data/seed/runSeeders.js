9cefa8ca157e68f4c93cbb21b63167a06e8277b8
tacokrydda <jbtand19@gmail.com>
2026-04-03 10:05:39 +0200

    fix name seedUsers

--- data/seed/runSeeders.js
+++ data/seed/runSeeders.js
@@ -1,6 +1,6 @@
 import { seedCampaignsIfEmpty } from "./seedCampaign.js";
 import { seedMenuIfEmpty } from "./seedMenu.js";
-import { seedUsersIfEmpty } from "./seedUrsers.js";
+import { seedUsersIfEmpty } from "./seedUsers.js";
 
 export function runSeeders() {
   seedMenuIfEmpty();
